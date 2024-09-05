325f275f5801de708e10779c9f39180754ffdaa2
miguelag99 <miguelag99@hotmail.com>
2024-09-05 09:55:22 +0200

    Fix: clean baseline config

--- prediction/configs/baseline.py
+++ prediction/configs/baseline.py
@@ -2,8 +2,8 @@ from types import SimpleNamespace
 
 baseline_cfg = SimpleNamespace(
     LOG_DIR = 'training_results',
-    TAG = 'prueba_resnet18',
-    WANDB_PROJECT = 'FAEFormer',
+    TAG = 'run_tag',
+    WANDB_PROJECT = 'wandb_project_name',
     WANDB_ID = '',
 
     ACCELERATOR = "cuda",
